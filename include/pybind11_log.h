0f4435dec1b0e15fb6f7a118d9d611e0ccf1a3a8
Haibao Tang <tanghaibao@gmail.com>
2022-12-31 15:42:21 -0800

    Remove cout calls

--- include/pybind11_log.h
+++ include/pybind11_log.h
@@ -1,20 +1,51 @@
 #pragma once
 
+#include <pybind11/pybind11.h>
 #include <spdlog/details/null_mutex.h>
 #include <spdlog/details/synchronous_factory.h>
 #include <spdlog/sinks/base_sink.h>
 #include <spdlog/spdlog.h>
 
-#include <iostream>
 #include <mutex>
 
+namespace py = pybind11;
+
 namespace pybind11_log {
+
+int map_level(spdlog::level::level_enum level) {
+  switch (level) {
+    case spdlog::level::trace:
+      return 5;
+    case spdlog::level::debug:
+      return 10;
+    case spdlog::level::info:
+      return 20;
+    case spdlog::level::warn:
+      return 30;
+    case spdlog::level::err:
+      return 40;
+    case spdlog::level::critical:
+      return 50;
+    case spdlog::level::off:
+    default:
+      return 0;
+  }
+}
 
 template <typename Mutex>
 class pybind11_sink : public spdlog::sinks::base_sink<Mutex> {
  public:
   void sink_it_(const spdlog::details::log_msg& msg) override {
-    std::cout << msg.payload.data() << std::endl;
+    std::string target = "pybind11_log";
+    auto py_logging = py::module::import("logging");
+    auto py_logger = py_logging.attr("getLogger")(target);
+    std::string filename = msg.source.filename ? msg.source.filename : "";
+    std::string msg_payload =
+        std::string(msg.payload.begin(), msg.payload.end());
+    auto record = py_logger.attr("makeRecord")(
+        target, map_level(msg.level), filename, msg.source.line, msg_payload,
+        py::none(), py::none());
+    py_logger.attr("handle")(record);
   }
 
   void flush_() override {}
@@ -35,8 +66,12 @@ SPDLOG_INLINE std::shared_ptr<spdlog::logger> pybind11_st(
   return Factory::template create<pybind11_sink_st>(logger_name);
 }
 
+class Logger {
+ public:
+ private:
+};
+
 void init() {
-  std::cout << "pybind11_log::init()" << std::endl;
   auto logger = pybind11_st("pybind11_log");
   spdlog::set_default_logger(logger);
 }
--- tests/pybind11_log_test.cpp
+++ tests/pybind11_log_test.cpp
@@ -8,8 +8,19 @@ int add(int i, int j) {
   return i + j;
 }
 
+int divide(int i, int j) {
+  if (j == 0) {
+    spdlog::error("Division by zero!");
+    throw std::invalid_argument("Division by zero!");
+  } else {
+    spdlog::info("divide({0}, {1})", i, j);
+  }
+  return i / j;
+}
+
 PYBIND11_MODULE(pybind11_log_test, m) {
   pybind11_log::init();
   m.doc() = "pybind11_log example plugin";
   m.def("add", &add, "A function which adds two numbers with logging");
+  m.def("divide", &divide, "A function which divides two numbers with logging");
 }
