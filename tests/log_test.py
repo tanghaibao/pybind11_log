580cfe98aed95cc4fd1e144ae13bccc3c1577e05
Haibao Tang <tanghaibao@gmail.com>
2024-11-01 17:40:36 -0700

    Pass in logger name (thanks @nradtke, #4) (#5)
    * Pass in a logger name
    
    * Add image and has name in the logging

--- tests/log_test.py
+++ tests/log_test.py
@@ -7,12 +7,13 @@ sys.path.append(os.getcwd())
 from rich.logging import RichHandler
 
 logging.basicConfig(
-    level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+    level="NOTSET", format="%(name)s %(message)s", datefmt="[%X]", handlers=[RichHandler()]
 )
 
 import pybind11_log_test
 
 if __name__ == "__main__":
+    pybind11_log_test.init("my_fancy_logger")
     pybind11_log_test.add(3, 4)
     pybind11_log_test.threaded_log()
     pybind11_log_test.divide(3, 0)
